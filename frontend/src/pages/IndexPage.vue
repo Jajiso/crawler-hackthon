a7f00bd6c13c8ef7d99b0f718e3d3b71099262ed
Jajiso <jajiso123@gmail.com>
2022-05-12 11:15:01 +0100

    created table for companies and added formated json with companies

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,28 +1,23 @@
 <template>
   <q-page class="">
-    <div v-for="(company, index) in companies" :key="index" class="q-ma-md">
-      <BussinessCard :company="company"/>
-    </div>
+    <CompaniesTable table_title="Website 1" :companies="website1_companies"/>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import BussinessCard from 'src/components/BussinessCard.vue'
+import CompaniesTable from 'src/components/CompaniesTable.vue'
 
 export default defineComponent({
   name: 'IndexPage',
-  components: {BussinessCard},
+  components: {CompaniesTable},
   data() {
     return {
-      companies: [
-        {title: 'company-1', location: 'London', website: 'https://www.google.com/', email: 'test@test.te'},
-        {title: 'company-2', location: 'London 2', website: 'https://www.google.com/', email: 'test@test.te'},
-        {title: 'company-3', location: 'London 3', website: 'https://www.google.com/', email: 'test@test.te'},
-        {title: 'company-4', location: 'London 4', website: 'https://www.google.com/', email: 'test@test.te'},
-        {title: 'company-5', location: 'London 5', website: 'https://www.google.com/', email: 'test@test.te'}
-      ]
+      website1_companies: null
     }
+  },
+  created() {
+    this.website1_companies = this.$companies1
   }
 
 })
