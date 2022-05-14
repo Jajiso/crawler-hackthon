85ae8e5e7eb41b35873f3b77781af2bf9e77d538
danielfetico <daniel@underpinned.co>
2022-05-14 13:41:58 +0300

    more range

--- Crawler/reed.py
+++ Crawler/reed.py
@@ -85,7 +85,7 @@ def scraping(webpage, page_number):
                 "Location": company_location
             }) 
             
-for x in range(1, 20): 
+for x in range(1, 100): 
     if x % 5 == 0:
         sleep(300)
     scraping('https://www.reed.co.uk/jobs/work-from-home-jobs?pageno=', x)
